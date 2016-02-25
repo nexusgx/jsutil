6431704fde38886ab0a6ccc17a0ad8f8db3240b1
nexusgx <steve.koehler@gmail.com>
2016-02-25 14:44:35 -0600

    change to use generic Storage object
    Added the loc property to allow any Storage object to be used. Defaults to localStorage

--- jsUtil.js
+++ jsUtil.js
@@ -1,40 +1,46 @@
 if(!jsUtil)
 var jsUtil={
-    //specific to localStorage
+    //specific to Storage objects
     local:{
-        
-        // convert val to JSON and add to local storage
+        loc:localStorage,
+        // convert val to JSON and add to the Storage object
         set:function(name,val){
             this['jsu_'+name]=val; // keep within jsUtil to bypass future JSON conversion
-            localStorage.setItem('jsu_'+name,JSON.stringify(val));
+            loc.setItem('jsu_'+name,JSON.stringify(val));
             return this;
         },
         
-        //retrieve the property from local storage
+        //retrieve the property from the Storage object
         get:function(name){
             var ret;
-            if(typeof this['jsu_'+name] === 'undefined') // look for a jsUtil copy to bypass JSON conversion
-                if(typeof localStorage.getItem('jsu_'+name)==='undefined')
+            if(typeof this['jsu_'+name] === 'undefined'){ // look for a jsUtil copy to bypass JSON conversion
+                if(this.exists(name)){
+                    ret=JSON.parse(loc.getItem('jsu_'+name));
+                }
+                else{
                     ret=null;
-                else
-                    ret=JSON.parse(localStorage.getItem('jsu_'+name));
-            else
+                }
+            }
+            else{
                 ret=this['jsu_'+name];
+            }
             return ret;
         },
         
-        //delete the property from local storage and jsUtil
+        //delete the property from the Storage object and jsUtil
         delete:function(name){
             delete this['jsu_'+name];
-            return delete localStorage['jsu_'+name];
+            return delete loc['jsu_'+name];
         },
         
-        //does the property exist in localStorage
+        //does the property exist in the Storage object; ignores the local properties
         exists:function(name){
-            if(typeof localStorage['jsu_'+name]==='undefined')
+            if(typeof loc['jsu_'+name]==='undefined'){
                 return false;
-            else
+            }
+            else{
                 return true;
+            }
         }
     }
 }
