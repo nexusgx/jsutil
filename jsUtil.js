e011cd326565a047795e620bf51b4818503e041e
nexusgx <steve.koehler@gmail.com>
2016-02-25 14:41:41 -0600

    added local.exists and local variable storage
    added local.exists to check for a property and local variable storage to bypass JSON conversion

--- jsUtil.js
+++ jsUtil.js
@@ -2,14 +2,39 @@ if(!jsUtil)
 var jsUtil={
     //specific to localStorage
     local:{
+        
+        // convert val to JSON and add to local storage
         set:function(name,val){
-            return localStorage.setItem(name,JSON.stringify(val));
+            this['jsu_'+name]=val; // keep within jsUtil to bypass future JSON conversion
+            localStorage.setItem('jsu_'+name,JSON.stringify(val));
+            return this;
         },
+        
+        //retrieve the property from local storage
         get:function(name){
-            return JSON.parse(localStorage.getItem(name));
+            var ret;
+            if(typeof this['jsu_'+name] === 'undefined') // look for a jsUtil copy to bypass JSON conversion
+                if(typeof localStorage.getItem('jsu_'+name)==='undefined')
+                    ret=null;
+                else
+                    ret=JSON.parse(localStorage.getItem('jsu_'+name));
+            else
+                ret=this['jsu_'+name];
+            return ret;
         },
+        
+        //delete the property from local storage and jsUtil
         delete:function(name){
-            return delete localStorage[name];
+            delete this['jsu_'+name];
+            return delete localStorage['jsu_'+name];
+        },
+        
+        //does the property exist in localStorage
+        exists:function(name){
+            if(typeof localStorage['jsu_'+name]==='undefined')
+                return false;
+            else
+                return true;
         }
     }
 }
