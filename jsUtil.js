1063cc827d3b8da3115ab80c755ce3ffa9013b26
nexusgx <steve.koehler@gmail.com>
2016-02-25 14:54:46 -0600

    change loc and local names
    Changed the names to make them more accurate and clearer for down the road

--- jsUtil.js
+++ jsUtil.js
@@ -1,12 +1,12 @@
 if(!jsUtil)
 var jsUtil={
     //specific to Storage objects
-    local:{
-        loc:localStorage,
+    store:{
+        storage:localStorage,
         // convert val to JSON and add to the Storage object
         set:function(name,val){
             this['jsu_'+name]=val; // keep within jsUtil to bypass future JSON conversion
-            loc.setItem('jsu_'+name,JSON.stringify(val));
+            storage.setItem('jsu_'+name,JSON.stringify(val));
             return this;
         },
         
@@ -15,7 +15,7 @@ var jsUtil={
             var ret;
             if(typeof this['jsu_'+name] === 'undefined'){ // look for a jsUtil copy to bypass JSON conversion
                 if(this.exists(name)){
-                    ret=JSON.parse(loc.getItem('jsu_'+name));
+                    ret=JSON.parse(storage.getItem('jsu_'+name));
                 }
                 else{
                     ret=null;
@@ -30,12 +30,12 @@ var jsUtil={
         //delete the property from the Storage object and jsUtil
         delete:function(name){
             delete this['jsu_'+name];
-            return delete loc['jsu_'+name];
+            return delete storage['jsu_'+name];
         },
         
         //does the property exist in the Storage object; ignores the local properties
         exists:function(name){
-            if(typeof loc['jsu_'+name]==='undefined'){
+            if(typeof storage['jsu_'+name]==='undefined'){
                 return false;
             }
             else{
